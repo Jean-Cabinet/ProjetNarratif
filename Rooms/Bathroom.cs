d73961eeae7a220d66ddd58424811b5463a3fec2
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-02 09:36:15 -0400

    Salon

--- Rooms/Bathroom.cs
+++ Rooms/Bathroom.cs
@@ -2,6 +2,7 @@
 {
     internal class Bathroom : Room
     {
+       internal static bool vapeur;
         internal override string CreateDescription() =>
 @"Dans la toilette, le [bain] est rempli d'eau chaude.
 Le [miroir] devant toi affiche ton visage déprimé.
@@ -13,10 +14,21 @@ Tu peux revenir dans ta [chambre].
             switch (choice)
             {
                 case "bain":
-                    Console.WriteLine("Tu te laisses relaxer dans le bain.");
+                    
+                    Console.WriteLine("Tu te laisses relaxer dans le bain. La vapeur monte");
+                    vapeur = true;
+                    
                     break;
                 case "miroir":
-                    Console.WriteLine("Tu aperçois les chiffres 2314 écrits sur la brume sur le miroir.");
+                    if (vapeur)
+                    {
+                        Console.WriteLine("Tu aperçois les chiffres 9632 écrits sur la brume sur le miroir.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("tu devrais te raser");
+                    }
+                    
                     break;
                 case "chambre":
                     Console.WriteLine("Tu retournes dans ta chambre.");

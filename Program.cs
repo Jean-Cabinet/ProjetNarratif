2647dfef4dcacc3fbe2cc639beacffffd7eae3df
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-16 10:41:21 -0500

    Progam

--- Program.cs
+++ Program.cs
@@ -1,19 +1,34 @@
 ﻿using ProjetNarratif;
-using ProjetNarratif.Rooms;
+using ProjetNarratif.Quests;
+using ProjetNarratif.Quête;
+
+
+ 
 
 var game = new Game();
-game.Add(new Bedroom());
-game.Add(new Bathroom());
-game.Add(new AtticRoom());
-game.Add(new LivingRoom());
+game.Add(new BeginQuest());
+game.Add(new Pnj());
+game.Add(new Riddle());
+game.Add(new Glossaire());
+game.Add(new Cadavre());
+game.Add(new Prime());
+game.Add(new Primehypno());
+game.Add(new SuiteRiddle());
+game.Add(new Riddlecadavre());
+game.Add(new Combat());
+game.Add(new Dilemme());
+game.Add(new Epargner());
+game.Add(new Sacrifier());
+
 while (!game.IsGameOver())
 {
     Console.WriteLine("--");
-    Console.WriteLine(game.CurrentRoomDescription);
+    Console.WriteLine(game.CurrentQuestDescription);
     string? choice = Console.ReadLine()?.ToLower() ?? "";
     Console.Clear();
     game.ReceiveChoice(choice);
 }
+
 
 Console.WriteLine("FIN");
 Console.ReadLine();

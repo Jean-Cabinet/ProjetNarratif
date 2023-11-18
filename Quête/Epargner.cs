edd88cd47d293024052e5aa4f5a217c91cf463a9
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-18 13:31:09 -0500

    Epargner

--- Quête/Epargner.cs
+++ Quête/Epargner.cs
@@ -13,7 +13,7 @@ namespace ProjetNarratif.Quête
  Régisse chèvre - Alors, vous avez abattu le monstre ?  
  Edvars de Ragn -  Oui, elle ne vous embêtera plus.
  Régisse chèvre - ahah, finalement vous etes plus sympathique vous autres
- Régisse chèvre - Tenez voici votre prime.
+ Régisse chèvre - Tenez voici votre prime.[Pr]
  ";
 
         internal override void ReceiveChoice(string choice)
@@ -26,17 +26,8 @@ namespace ProjetNarratif.Quête
                     Console.WriteLine("Il savait très bien que épargner un monstre, faisait de lui un renégat qui sera poursuit par des menbres de sa propre guilde pour toucher la mise à prime");
                     Console.WriteLine("Edvars monta à dos de son cheval pour atteindre le prochain village pour résoudre une nouvellle contrat, en tant que rénégat et non Croiseleurs");
                     
-
-
                     break;
 
-                case "PRE":
-                    //dialogue Prime et hérédité prime
-                    Console.WriteLine("Edvars pris la prime, et s'en alla, s'en dire un mot");
-                    Console.WriteLine("Il savait très bien que épargner un monstre, faisait de lui un renégat qui sera poursuit par des menbres de sa propre guilde pour toucher la mise à prime");
-                    Console.WriteLine("Edvars monta à dos de son cheval pour atteindre le prochain village pour résoudre une nouvellle contrat, en tant que rénégat et non Croiseleurs");
-
-                    break;
 
 
 

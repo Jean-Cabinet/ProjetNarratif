3cba8e9fd9f41ddce36cabe61942bb976c5579d9
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-17 21:29:09 -0500

    Fin_alternatif

---
+++ Quête/Fin_alternatif.cs
@@ -0,0 +1,24 @@
+﻿using ProjetNarratif.Quests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Quête
+{
+    internal class Fin_alternatif : Quest
+    {
+        internal override string CreateDescription() =>
+  @"                                         VOUS ETES MORT
+Edvars avait le présentitiment que son erreur dans l'identification du monstre, pourrais lui coter la vie.........
+ Je pensais que vous auriez été meilleure.......Ce n'est pas comme ça que vous allez tuer Aldagusar voyons.......
+  reprenons à la page Glossaire et tachez de bien choisir";
+
+        internal override void ReceiveChoice(string choice)
+        {
+            Game.Transition<Glossaire>();
+
+        }
+    }
+}

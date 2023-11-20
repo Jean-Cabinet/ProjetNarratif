04ee3df720c645b485239c19f7dc24a2ad3266dc
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-20 16:18:17 -0500

    lycan

---
+++ Quête/Huilelycan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Quête
+{
+    internal class Huilelycan : Item
+    {
+        public int huilelycan = 0;
+
+
+        public Huilelycan(string name, string type, int huilelycan)
+        {
+            this.name = name;
+            this.type = type;
+            this.huilelycan = huilelycan;
+
+
+        }
+
+
+
+
+
+    }
+}

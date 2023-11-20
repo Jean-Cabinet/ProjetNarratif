e6a57f70a7004bfce83cb5b4b6150f00400df042
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-20 16:17:51 -0500

    Hirondelle

---
+++ Quête/Hirondelle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Quête
+{
+    internal class Hirondelle : Item
+    {
+        public int HealingAmount = 12;
+
+
+
+
+        public Hirondelle(string name, string type, int HealingAmount)
+        {
+            this.name = name;
+            this.type = type;
+            this.HealingAmount = HealingAmount;
+
+
+        }
+    }
+}

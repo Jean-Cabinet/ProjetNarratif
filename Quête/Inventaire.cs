28f1c94ef3590ca6e16cc41a71e19fa3dd70002a
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-20 14:54:13 -0500

    Inventaire

---
+++ Quête/Inventaire.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Quête
+{
+    internal class Inventaire
+    {
+        List<Item> inventaire = new();
+
+        public Inventaire() 
+        { 
+         
+
+        }
+
+        public void Add (Item item)
+        {
+            inventaire.Add(item);
+        }
+
+        public void Sub(Item item)
+        {
+            inventaire.Remove(item);
+        }
+
+        public void See (Item item)
+        {
+            inventaire.ForEach(i => Console.WriteLine(i.name));
+        }
+
+
+        
+    }
+}

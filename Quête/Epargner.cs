44f63ef9d8b673851bc7826648ea7f46652dfc49
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-20 14:54:55 -0500

    Epargner

--- Quête/Epargner.cs
+++ Quête/Epargner.cs
@@ -21,7 +21,7 @@ namespace ProjetNarratif.Quête
             switch (choice)
             {
                 case "Pr":
-                    //dialogue Prime et hérédité prime
+                    
                     Console.WriteLine("Edvars pris la prime, et s'en alla, s'en dire un mot");
                     Console.WriteLine("Il savait très bien que épargner un monstre, faisait de lui un renégat qui sera poursuit par des menbres de sa propre guilde pour toucher la mise à prime");
                     Console.WriteLine("Edvars monta à dos de son cheval pour atteindre le prochain village pour résoudre une nouvellle contrat, en tant que rénégat et non Croiseleurs");

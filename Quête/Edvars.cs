1cffc67059980f1e3ef37a513c90298b6f4c48dd
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-20 14:52:39 -0500

    Statistiques edvars

---
+++ Quête/Edvars.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Quête
+{
+        internal class Edvars
+        {
+            public int PVEdvars { get; set; }
+            public int Gold { get; set; }
+            public int PointexperienceSacrifice { get; set; }
+            public int PointexperienceEpargner { get; set; }
+            public int levelSacrifice { get; set; }
+            public int levelEpargner { get; set; }
+            public Inventaire inventaire {  get; set; } 
+         public Edvars() 
+         { 
+            this.PVEdvars = 50;
+            this.Gold = 10;
+            this.levelEpargner = 1;
+            this.levelSacrifice = 1;
+            this.PointexperienceSacrifice = 0;
+            this.PointexperienceSacrifice = 0;
+            this.inventaire = new Inventaire();
+            this.inventaire.Add(new Flamberge("Gwynbleidd", "flamberge", 12));
+
+                            
+         }   
+        }
+
+
+
+
+    
+}

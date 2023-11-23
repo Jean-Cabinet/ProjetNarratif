3b3790f57342810d5ad29e4afff47b4ce7eeb13c
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-23 11:09:35 -0500

    Village

--- Quête/VillageS.cs
+++ Quête/VillageS.cs
@@ -9,7 +9,11 @@ namespace ProjetNarratif.Quête
     internal class VillageS : Quest
     {
         internal override string CreateDescription() =>
-       @"";
+       @"Sur la route pour le prochain village acommpagné de son fièle destrier,Edvars fut troubler par ce qui lui était arriver, il avait sentit une puissance destructice s'emparer de lui. 
+         Tout en regardant son arme,il vit que celle-ci avait pris une teinte noiratre avec des reflets rouges. Edvars savait que son arme etait un artefact magique parmis tant d'autres
+         qu'il fournissait à l'école des croiseleurs et que lui avait décidé de prendre une flamberge.
+         Edvars de Ragn - Tuer et ne pas succomber......hmm ça prends du sens maintenant
+         Edvars de Ragn - Je devrais me reposer ce combat m'a épuisé, allons à l'[auberge] dans ce village.";
         internal override void ReceiveChoice(string choice, Edvars e, Leshen l)
         {
 
--- Quête/VillageÉ.cs
+++ Quête/VillageÉ.cs
@@ -9,7 +9,14 @@ namespace ProjetNarratif.Quête
     internal class VillageÉ : Quest
     {
         internal override string CreateDescription() =>
-        @"";
+        @"Sur la route pour le prochain village acommpagné de son fièle destrier,Edvars repensa à son choix d'épargner cette femme qui autre fois comme lui était une croiseleuses.
+         On lui avait appris dans son entrainement intense à ne pas céder à la pitié, en lui bourrant dans le crâne : 
+         aucun monstre ne doit survivre. La personne qui se transforme ne conserve pas ses pleins pouvoirs, et est obligé de vivre dans un monde qui ne veut pas d'elle,
+         soit parmis les hommes ou la guilde d'avalon. Conservant les marques sur son corps de son échec et de son passé. Ces personnes là finissent par devenir ronin,
+         qui essaye après leur échec se voir faire accepter pour faire un contrat, n'étant plus affilié à aucune guilde. Il peuvent se retransformer en monstre si cette de ronin
+         les pousse ans leur derniers retranchement.
+         Edvars de Ragn -  Gwynbleidd.....elle a pris une teinte blanchâtre, rayonne avec un légère intensité
+         Edvars de Ragn - Je devrais me reposer ce combat m'a épuisé, allons à l'auberge dans ce village.";
         internal override void ReceiveChoice(string choice, Edvars e, Leshen l)
         {
 

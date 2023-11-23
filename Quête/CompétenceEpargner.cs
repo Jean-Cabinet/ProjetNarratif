3b87df7fc93add79345f9763f4789597f7c34f9e
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-22 21:35:42 -0500

    Arbre compétences

---
+++ Quête/CompétenceEpargner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Quête
+{
+    internal class CompétenceEpargner
+    {
+    }
+}

7d4e47a12caf0e9bf7c32432e77d7ecc16fe04f8
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-27 14:04:36 -0500

    Auberge

---
+++ Quête/AubergeS.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetNarratif.Quête
+{
+    internal class AubergeS : Quest
+    {
+
+        internal override string CreateDescription() =>
+ @"";
+        
+        internal override void ReceiveChoice(string choice, Edvars e)
+        {
+            e.compétences = new CompétenceSacrifice("Mort destinée","oui",20);
+            switch (choice)
+            {
+                case "Pr":
+
+                    
+
+                    break;
+
+
+
+
+
+            }
+
+
+
+        }
+    }
+
+
+
+
+
+
+}
+}

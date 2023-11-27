32403d60800be41238fb36e624549707f0a7da35
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-27 17:32:18 -0500

    Auberge

--- Quête/AubergeE.cs
+++ Quête/AubergeE.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace ProjetNarratif.Quête
 {
-    internal class AubergeE
+    internal class AubergeE : Quest
     {
+        internal override string CreateDescription() =>
+     @"Une fois dans sa chambre, edvars déposa son matériel au sol. Il repensa à cette quête, tout en regardant son arme
+     qui avait changer de teinte....Edvars se leva et prenna son épée. Il sentit une puissance envahir son bras droit
+     celui - ci se mit à muter légére comme une sorte d'écaille d'un pureté rayonnate recouvrit l'avant bras d'Edvars
+      Edavrs de Ragn - Hmm... intéressant, il semblerait que cette épée soit que ce que je le pensais. 
+      Je me demande si je continuais à épargner ce monstre si mon arme et mon bras vont continuer de changer.
+      Edavrs de Ragn - Sortons, je n'arriverais à dormir de toutes évidence.[dehors]";
+
+        internal override void ReceiveChoice(string choice, Edvars e)
+        {
+            e.levelEpargner = e.levelEpargner + 1;
+            Console.WriteLine($"vous avez atteint le niveau {e.levelEpargner} en épargnement");
+            e.PVEdvars = e.PVEdvars + 5;
+            e.absorptionquen = e.absorptionquen + 5;
+            Console.WriteLine($"Votre sortilège de bouclier absorbe plus dé dégats : {e.absorptionquen}");
+            Console.WriteLine($"Vos nombres de point de vie, on augmenter de 5 est passe à {e.PVEdvars}");
+            e.compétences = new CompétenceSacrifice("Jugement destinée", @"libère  le pouvoir des purificateur dans son arme, en plongeant l'épée dans le sol pour invoquer une myriade de lames. 
+             En plus d'infliger des dégâts immédiats, cette attaque augmente la défense du personnage pendant un long moment.", 20);
+            Console.WriteLine($"Vous apprenez uen nouvelle Jugement destinée : {e.compétences} ");
+            switch (choice)
+            {
+                case "dehors":
+
+                    Console.WriteLine("Sans un bruit edvars sortit de l'auberge et prena son cheval en direction de la prochaine ville, Berg.");
+
+
+                    break;
+
+
+
+
+
+            }
+
+
+
+        }
     }
 }
--- Quête/AubergeS.cs
+++ Quête/AubergeS.cs
@@ -10,16 +10,31 @@ namespace ProjetNarratif.Quête
     {
 
         internal override string CreateDescription() =>
- @"";
+ @"Une fois dans sa chambre, edvars déposa son matériel au sol. Il repensa à cette quête, tout en regardant son arme
+     qui avait changer de teinte....Edvars se leva et prenna son épée. Il sentit une puissance envahir son bras droit
+     celui - ci se mit à muter légérement comme une sorte de peau d'un noir sombre parcouru de veines d'un rouge sanguinolant recouvrit l'avant bras d'Edvars.
+     Edvars cria, il sentit l'ame du monstre qui avait sacrifier lui parourir le bras.
+      Edavrs de Ragn - *soupir avec un souflle coupé*, qu'est ce que ?.......
+      Edvars vit son bras et le regarda avec attention, il sentit son coeur battre dans son avant-bras. Edvars entendit plusieurs pas de diriger vers sa chambre.
+      Il prena ses affaires et quitta l'auberge par le fenêtre et partit avec son [cheval]. ";
         
         internal override void ReceiveChoice(string choice, Edvars e)
         {
-            e.compétences = new CompétenceSacrifice("Mort destinée","oui",20);
+            e.levelSacrifice = e.levelSacrifice + 1;
+            Console.WriteLine($"vous avez atteint le niveau {e.levelSacrifice} en sacrifice");
+            e.Attaqueignis = e.Attaqueignis + 5;
+            e.Attaquefrostis = e.Attaquefrostis + 5;
+            Console.WriteLine($"Vos sortilèges offensif sont plus puissant : Ignis est passé de 5 à {e.Attaqueignis} et frostis est passé de 5 à {e.Attaquefrostis}");
+            e.compétences = new CompétenceSacrifice("Mort destinée", @"Edvars à l'aide de sa flamberge libère le pouvoir des ames emprisonner dans son arme,
+             en plongeant l'épée dans le sol pour invoquer une myriade de lames. En plus d'infliger des dégâts immédiats, cette attaque réduit la défense de la cible pendant un instant.", 20);
+            Console.WriteLine($"Vous apprenez une nouvelle compétence Mort destinée : {e.compétences} ");
             switch (choice)
             {
-                case "Pr":
+                case "cheval":
 
-                    
+
+                    Console.WriteLine("Edvars de Ragn - Direction la capitale de berg. Ya au galop");
+
 
                     break;
 
@@ -40,4 +55,4 @@ namespace ProjetNarratif.Quête
 
 
 }
-}
+

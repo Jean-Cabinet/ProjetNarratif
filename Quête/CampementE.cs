80ee4ec3e207115e491aabcd80b32cafa4a3ad54
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-11-30 09:37:24 -0500

    Nouvelle quete

--- Quête/CampementE.cs
+++ Quête/CampementE.cs
@@ -9,11 +9,45 @@ namespace ProjetNarratif.Quête
     internal class CampementE : Quest
     {
         internal override string CreateDescription() =>
-     @"";
+     @"Après c'etre fait expulsé de l'auberge, Edvars cherchait un coin paisible pour dormir à la belle étoile.
+       Au bord du feu de camp, Edvars regarda les flammes du feu, repensait à son action et ce que cela impliquait d'etre un renégat. 
+       Il serait pourchassé par un groupe chevronnée de croiseleurs envoyé par la guilde d'Avalon. Edvars reporta son regard sur son épée.
+       La flamberge etait devenu d'un acier blanc éclatant presque lumineux après avoir sauvé leshen.Edvars prenna son arme et sentit une puissance envahir son bras droit
+     celui - ci se mit à muter légére, une sorte d'écaille d'une pureté rayonnate recouvrit l'avant bras d'Edvars
+      Edavrs de Ragn - Hmm... intéressant, il semblerait que cette épée soit plus que ce que je ne pensais. 
+      Je me demande si je continuais à épargner ce monstre si mon arme et mon bras vont continuer de changer.
+      Edavrs de Ragn - Il faut que je dorme, le voyage sera long demain.
+      A l'aube du matin, edvars sentint une groupe de personne se rappocher de lui, il continua de faire semblant de dormir. [combat]";
 
         internal override void ReceiveChoice(string choice, Edvars e)
         {
-            
+            switch (choice)
+            {
+                case "combat":
+
+                    e.levelEpargner = e.levelEpargner + 1;
+                    Console.WriteLine($"vous avez atteint le niveau {e.levelEpargner} en épargnement");
+                    e.PVEdvars = e.PVEdvars + 5;
+                    e.absorptionquen = e.absorptionquen + 5;
+                    Console.WriteLine($"Votre sortilège de bouclier absorbe plus dé dégats : {e.absorptionquen}");
+                    Console.WriteLine($"Vos nombres de point de vie, on augmenter de 5 est passe à {e.PVEdvars}");
+                    e.compétences = new CompétenceSacrifice("Jugement destinée", @"libère  le pouvoir des purificateur dans son arme, en plongeant l'épée dans le sol pour invoquer une myriade de lames. 
+                     En plus d'infliger des dégâts immédiats, cette attaque augmente la défense du personnage pendant un long moment.", 20);
+                    Console.WriteLine($"Vous apprenez une nouvelle Jugement destinée : {e.compétences} ");
+                    Thread.Sleep(2000); 
+
+
+
+
+
+
+
+
+
+                    break;
+
+
+            }
 
 
 
--- Quête/CampementS.cs
+++ Quête/CampementS.cs
@@ -9,11 +9,44 @@ namespace ProjetNarratif.Quête
     internal class CampementS : Quest
     {
         internal override string CreateDescription() =>
-     @"";
+     @"Après c'etre fait expulsé de l'auberge, Edvars cherchait un coin paisible pour dormir à la belle étoile.
+       Au bord du feu de camp, Edvars regarda les flammes du feu, repensait à son action, il entendit des voix émanée de son épée, on aurait dit  les plaintes du leshen....
+       . Un bruit de coeur qui bat s'intensifia dans l'esprit d'edvars, en regardant son épée. Il décida de la prendre en main.
+       La flamberge etait devenu d'un acier noir sinistre refletant un rouge sanguinolant.Edvars prenna son arme et sentit une puissance envahir son bras droit,
+     celui - ci se mit à muter légére, une sorte de peau d'un noir sombre parcouru de veines d'un rouge sanguinolant recouvrit l'avant bras d'Edvars.
+      Edvars cria, il sentit l'ame du monstre qu'il avait sacrifier lui parourir le bras.
+      Edavrs de Ragn - *soupir avec un souflle coupé*, qu'est ce que ?.......
+      Edvars vit son bras et le regarda avec attention, il sentit son coeur battre dans son avant-bras.
+      Edvars ne ferma pas les yeux du soir, obnubiler par son avant-bras et les battements de coeur dans son bras qui le lançait.
+      A l'aube du matin, edvars sentit une groupe de personne se rappocher de lui, il faisa semblant de dormir. [combat]";
 
         internal override void ReceiveChoice(string choice, Edvars e)
         {
-           
+           switch(choice)
+           {
+
+                case "combat":
+
+                    e.levelSacrifice = e.levelSacrifice + 1;
+                    Console.WriteLine($"vous avez atteint le niveau {e.levelSacrifice} en sacrifice");
+                    e.Attaqueignis = e.Attaqueignis + 5;
+                    e.Attaquefrostis = e.Attaquefrostis + 5;
+                    Console.WriteLine($"Vos sortilèges offensif sont plus puissant : Ignis est passé de 5 à {e.Attaqueignis} et frostis est passé de 5 à {e.Attaquefrostis}");
+                    e.compétences = new CompétenceSacrifice("Mort destinée", @"Edvars à l'aide de sa flamberge libère le pouvoir des ames emprisonner dans son arme,
+                    en plongeant l'épée dans le sol pour invoquer une myriade de lames. En plus d'infliger des dégâts immédiats, cette attaque réduit la défense de la cible pendant un instant.", 20);
+                    Console.WriteLine($"Vous apprenez une nouvelle compétence Mort destinée : {e.compétences} ");
+
+
+
+
+
+
+
+                    break;
+
+
+
+           }
 
 
 

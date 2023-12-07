c19133433000c29ed4a2bf027f92de68dc7a25ec
Jean-Cabinet <nathan.isarte@lcieducation.net>
2023-12-07 11:20:08 -0500

    correction engime

--- Quête/Enigme.cs
+++ Quête/Enigme.cs
@@ -10,7 +10,7 @@ namespace ProjetNarratif.Quête
     {
 
         internal override string CreateDescription() =>
-@" [enigme]";
+@"[enigme]";
 
 
         internal override void ReceiveChoice(string choice, Edvars e)
@@ -21,32 +21,47 @@ namespace ProjetNarratif.Quête
 
 
                     int essais = 0;
-                    string réponse;
+                    string réponse = "";
+                    bool bonneréponse = false;
+                    
                     do
                     {
-                        Console.WriteLine("Que dit une noix qui éternue ?");
-                        réponse = Console.ReadLine();
-                        if (essais == 1)
+                        if (bonneréponse == false)
+                        {
+                            Console.WriteLine("Que dit une noix qui éternue ?");
+                            réponse = Console.ReadLine();
+                            if (réponse != "cajou")
+                            {
+                                essais++;
+                            }
+                        }
+                        if (essais == 1 && bonneréponse == false)
                         {
                             Console.WriteLine(" 1 ère indice : originaire d'amerique tropicale");
-
+                            
                         }
-                        if (essais == 2)
+                        if (essais == 2 && bonneréponse == false)
                         {
                             Console.WriteLine(" 2 ème indice : son deuxième nom est anacarde");
-
+                            
                         }
-                        if (réponse =="cajou")
+                        if (réponse =="cajou" && bonneréponse == false)
                         {
+                            bonneréponse = true;
                             Console.WriteLine("Bravo vous trouvé la réponse");
 
                         }
+                        if(essais == 3 && bonneréponse == false)
+                        {
+                            Console.WriteLine("Vous n'avez pas trouvé la bonne réponse");
+                            
+                        }
 
 
 
-                    } while (essais != 2);
+                    } while (essais != 3);
 
-                    Console.WriteLine("Vous n'avez pas trouvé la bonne réponse");
+                    
 
 
 
